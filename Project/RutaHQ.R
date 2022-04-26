d37f4c19ee5353587c0b27aa9f44e660b5467ea4
Junior Antonio Calvo Montañez <junior.calvo@unmsm.edu.pe>
2022-04-26 18:06:38 -0500

    code

--- Project/RutaHQ.R
+++ Project/RutaHQ.R
@@ -6,8 +6,20 @@ library(sf)
 library(cptcity)
 library(htmltools)
 
-#Lectura de datos 
-setwd('E:/MUSHO/Ciclismo/Nombres_Rutas')
+#LECTURA DE DATOS
 
-rut <- st_read('ruta_Huampu_Qotu.gpkg')
-plot(rut)
+#Rutas de Huampu Qotu de Musho
+rut_line <- st_read('https://github.com/junior0428/RutasMusho/blob/main/vectorial/ruta_Huampu_Qotu.gpkg?raw=true')
+plot(rut_line)
+
+#Nombre de la ruta de Huampu Qotu de Musho
+rut_nom <- st_read("https://github.com/junior0428/RutasMusho/blob/main/vectorial/nombre_Huampu_Qotu.gpkg?raw=true")
+plot(st_geometry(rut_nom), add = T)
+
+
+
+#CREACION DE MAPA WEB
+rut_nom %>% 
+  leaflet() %>% 
+  addProviderTiles(provider = providers$CartoDB.DarkMatter) %>% 
+  addPolygons(fillColor = "green", highlight = highlightOptions( weight = 5, color = 'red', fillOpacity = 0.7, bringToFront = TRUE))
